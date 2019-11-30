4883d50628668830eff1826497f05f7b5f84aa4e
Leon120520209 <57053546+Leon120520209@users.noreply.github.com>
2019-11-30 00:32:50 -0800

    Update Shape.h
    complete abstract class Shape

--- Shape.h
+++ Shape.h
@@ -8,38 +8,90 @@
 #ifndef SHAPE_H_
 #define SHAPE_H_
 
-
 #include <iostream>
 #include <iomanip>
 #include <QPainter>
+#include <QPen>
+#include <QBrush>
 
 using namespace std;
 
 enum class ShapeType{
-	Line, Polyline, Polygon, Rectangle, Ellipse, Text
-};
-
-
+		Line, Polyline, Polygon, Rectangle, Ellipse, Text, NoShape
+		};
 
 class Shape
 {
 	public:
-		Shape();//constructor
-		virtual ~Shape();//destructor
+	    
+		Shape(QPaintDevice* device = nullptr,int id = -1, ShapeType shape = NoShape);//constructor
+		virtual ~Shape(){};//destructor
+		
+		ShapeType getShape() const;
+		const QPen& getPen() const;
+		const QBrush& getBrush() const;
+		
+		void setShape(ShapeType shape);
+		void setPen(Qt::GlobalColor,int width,Qt::Penstyle,Qt::PenCapStyle,Qt::PenJoinStyle);
+		void setPen(Qt::GlobalColor);
+		void setBrush(Qt::GlobalColor,Qt::BrushStyle);
+		
+		
 		//pure virtual funstions:
 		virtual void draw(const int translate_x,const int translate_y) = 0;
 		virtual void move(const int x,const int y) = 0;
 		virtual float perimeter() = 0;
 		virtual float area() = 0; 
+	
+	protected:
+		QPainter& getQpainter();
+		
 	private:
 		int shapeID;		
 		QPainter qpainter;
 		ShapeType shape;
-		QPen pen;
-		QBrush brush;
+		QPen qpen;
+		QBrush qbrush;
 };
 
+ShapeType Shape::getShape() const
+{
+	return shape;
+}
 
+const QPen& Shape::getPen() const
+{
+	return qpen;
+}
 
+const QBrush& Shape::getBrush() const
+{
+	return qbrush;
+}
 
+void Shape::setBrush(Qt::GlobalColor color,Qt::BrushStyle style)
+{
+	qbrush.setColor(color);
+	qbrush.setStyle(style);
+}
+
+void Shape::setPen(Qt::GlobalColor color,int width,Qt::Penstyle style,Qt::PenCapStyle cstyle,Qt::PenJoinStyle jstyle)
+{
+	qpen.setColor(color);
+	qpen.setWidth(width);
+	qpen.setStyle(style);
+	qpen.setCapStyle(cstyle);
+	qpen.setJoinStyle(jstyle);
+}
+
+void Shape::setPen(Qt::GlobalColor color)
+{
+	qpen.setColor(color);
+}
+
+void Shape::setShape(ShapeType s)
+{
+	shape = s;
+}
 #endif /* SHAPE_H_ */
+

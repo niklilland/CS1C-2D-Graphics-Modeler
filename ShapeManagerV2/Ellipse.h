51ab166a8c7520884bde9b700cbecddfb601bfbe
Leon120520209 <57053546+Leon120520209@users.noreply.github.com>
2019-12-06 16:19:16 -0800

    added calculate functions for perimeter and area

--- ShapeManagerV2/Ellipse.h
+++ ShapeManagerV2/Ellipse.h
@@ -1,8 +1,9 @@
 /*
  * Ellipse.h
  *
- *  Created on: 2019��12��4��
- *      Author: Leon
+ *  Created on: 2019
+ *
+ *  Author: Leon
  */
 
 #ifndef ELLIPSE_H_
@@ -22,6 +23,8 @@ class Ellipse : public Shape
 		bool isCircle() const;
 		void set_rect(const QRect& rect);
         void draw(const int translate_x = 0, const int translate_y = 0) override;
+		float perimeter() override;
+        float area() override;
 	private:
         QRect rect;
 };
@@ -39,5 +42,15 @@ void Ellipse::set_rect(const QRect& rect)
 void Ellipse::draw(const int translate_x, const int translate_y)
 {
     qpainter.drawEllipse(translate_x, translate_y, rect.width(), rect.height());
+}
+
+float Ellipse::perimeter()
+{
+	return 2*3.14 * sqrt( (pow(0.5*rect.width(),2) + pow(0.5*rect.length(),2)) / 2 );
+}
+
+float Ellipse::area()
+{
+	return 3.14 *0.5*(rect.width()) *0.5*rect.length() ;
 }
 #endif /* ELLIPSE_H_ */
